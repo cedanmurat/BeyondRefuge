49c517f87ddb78771d56cac9875b753fc8eed25c
Cedan <cmzeynepli@gmail.com>
2018-01-30 22:57:38 +0300

    Beyond Refuge First Upload

---
+++ app/src/main/java/com/example/recodedcedan/beyondrefuge/SelfPacedLearningActivity.java
@@ -0,0 +1,13 @@
+package com.example.recodedcedan.beyondrefuge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelfPacedLearningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_paced_learning);
+    }
+}
